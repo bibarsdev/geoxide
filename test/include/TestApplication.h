e18c7e49ed3fc2adb2390bf37c41a45c59278003
bibarsdev <cheikhanas924@gmail.com>
2022-12-08 23:19:07 +0100

    Removed scene system

--- test/include/TestApplication.h
+++ test/include/TestApplication.h
@@ -4,37 +4,13 @@
 
 #include <Geoxide.h>
 
-#define SAFE_DELETE(var) if (var) { delete var; var = 0; }
-
 using namespace Geoxide;
-
-class TestApplication;
-
-class Scene
-{
-public:
-	Scene() : mApp(0), mScnMan(0) {}
-	Scene(TestApplication* app);
-	virtual ~Scene() = default;
-
-	virtual void prepareScene() = 0;
-	virtual void destroyScene() = 0;
-
-	virtual void onFrameStart(FrameEvent*) = 0;
-	virtual void onFrameEnd(FrameEvent*) = 0;
-	virtual void onImGui(FrameEvent*) = 0;
-	
-protected:
-	TestApplication* mApp;
-	ResourceManager* mResMan;
-	SceneManager* mScnMan;
-};
 
 class TestApplication : public Application
 {
 public:
-	TestApplication() : mCurrentScene(0), Application("TestApplication") {}
-	~TestApplication();
+	TestApplication() : Application("TestApplication") {}
+	~TestApplication() {}
 
 	void start();
 
@@ -44,11 +20,18 @@ public:
 	void onKeyUp(KeyUpEvent* e) override;
 
 public:
-	void enterAndLoadScene();
+	void prepareScene();
 
 private:
-	std::unordered_map<std::string, Scene*> mScenes;
-	Scene* mCurrentScene;
+	Vector mBackColor;
+	Vector mCameraPosition;
+	Vector mCameraOrientation;
+	Vector mEntityPosition;
+	Vector mEntityOrientation;
+	Vector mEntityScaling;
+	std::unordered_map<std::string, MeshEntity> mEntities;
+	std::vector<const char*> mEntityNames;
+	SceneNode* mNode;
 };
 
 #endif
--- test/src/TestApplication.cc
+++ test/src/TestApplication.cc
@@ -1,15 +1,12 @@
 
 #include "TestApplication.h"
 
-#include "SandboxScene.h"
+#define DEFAULT_CAMERA_POSITION NewVector(0, 5, -10)
+#define DEFAULT_CAMERA_ORIENTATION NewVector(0, 0, 0)
 
-Scene::Scene(TestApplication* app) : mApp(app), mResMan(mApp->getResourceManager()), mScnMan(mApp->getSceneManager()) {}
-
-TestApplication::~TestApplication()
-{
-	for (auto& scene : mScenes)
-		delete scene.second;
-}
+#define DEFAULT_ENTITY_POSITION NewVector(0, 0, 0)
+#define DEFAULT_ENTITY_ORIENTATION NewVector(0, 0, 0)
+#define DEFAULT_ENTITY_SCALING NewVector(1, 1, 1)
 
 void TestApplication::start()
 {
@@ -26,10 +23,42 @@ void TestApplication::start()
 
 	initialize(appInit);
 
-	// initialize scenes
-	mScenes["Sandbox"] = new SandboxScene(this);
+	prepareScene();
 
-	enterAndLoadScene();
+	startRendering();
+}
+
+void TestApplication::prepareScene()
+{
+	mBackColor = NewVector(0.2f, 0.2f, 0.8f, 1);
+
+	mCameraPosition = DEFAULT_CAMERA_POSITION;
+	mCameraOrientation = DEFAULT_CAMERA_ORIENTATION;
+
+	mEntityPosition = DEFAULT_ENTITY_POSITION;
+	mEntityOrientation = DEFAULT_ENTITY_ORIENTATION;
+	mEntityScaling = DEFAULT_ENTITY_SCALING;
+
+	Mesh* bunnyMesh = mResMan.loadModel("bunny.model");
+	Mesh* lucyMesh = mResMan.loadModel("lucy.model");
+	Mesh* teapotMesh = mResMan.loadModel("teapot.model");
+	Mesh* linkStatueMesh = mResMan.loadModel("link_statue.model");
+
+	mEntities["Bunny"] = MeshEntity(bunnyMesh);
+	mEntities["Lucy"] = MeshEntity(lucyMesh);
+	mEntities["Teapot"] = MeshEntity(teapotMesh);
+	mEntities["Link Statue"] = MeshEntity(linkStatueMesh);
+
+	for (auto& entity : mEntities)
+	{
+		mEntityNames.push_back(entity.first.c_str());
+	}
+
+	mNode = mScnMan.getRootNode()->newChild();
+
+	mNode->setEntity(&mEntities.at("Bunny"));
+
+	startRendering();
 }
 
 void TestApplication::onKeyUp(KeyUpEvent* e)
@@ -40,69 +69,100 @@ void TestApplication::onKeyUp(KeyUpEvent* e)
 	{
 	case kKeyCodeEscape:
 		stopRendering();
-
-		mCurrentScene->destroyScene();
-
-		mScnMan.getRootNode()->clearChildren();
-		mResMan.freeAllResources();
-
-		enterAndLoadScene();
 		break;
 	default:
 		break;
 	}
 }
 
-void TestApplication::enterAndLoadScene()
-{
-	std::string scenesMsg = "Available scenes:";
-
-	for (auto& scene : mScenes)
-		scenesMsg = scenesMsg + " " + scene.first;
-
-	Log::Info(scenesMsg);
-
-EnterSceneName:
-	std::cout << "Enter scene to load: ";
-
-	std::string selectedScene;
-	std::cin >> selectedScene;
-
-	auto& iter = mScenes.find(selectedScene);
-
-	if (iter == mScenes.end())
-	{
-		Log::Error("Unknown Scene \"" + selectedScene + "\"");
-		goto EnterSceneName;
-	}
-
-	Scene* loadedScene = iter->second;
-	loadedScene->prepareScene();
-
-	mCurrentScene = loadedScene;
-
-	Log::Info("Loaded Scene \"" + selectedScene + "\"");
-
-	startRendering();
-}
-
 void TestApplication::onFrameStart(FrameEvent* e)
 {
 	Application::onFrameStart(e);
 
-	mCurrentScene->onFrameStart(e);
+	mScnMan.setBackColor(mBackColor);
+
+	mScnMan.getMainCamera()->setPosition(mCameraPosition);
+	mScnMan.getMainCamera()->setOrientation(mCameraOrientation);
+
+	MeshEntity* entity = (MeshEntity*)mNode->getEntity();
+
+	entity->setPosition(mEntityPosition);
+	entity->setOrientation(mEntityOrientation);
+	entity->setScaling(mEntityScaling);
 }
 
 void TestApplication::onFrameEnd(FrameEvent* e)
 {
 	Application::onFrameEnd(e);
-
-	mCurrentScene->onFrameEnd(e);
 }
 
 void TestApplication::onImGui(FrameEvent* e)
 {
 	Application::onImGui(e);
+	if (ImGui::Begin("World"))
+	{
+		ImGui::ColorPicker4("Background Color", (float*)&mBackColor);
+	}
+	ImGui::End();
 
-	mCurrentScene->onImGui(e);
+	if (ImGui::Begin("Camera"))
+	{
+		ImGui::DragFloat("X", &VectorXRef(mCameraPosition));
+		ImGui::DragFloat("Y", &VectorYRef(mCameraPosition));
+		ImGui::DragFloat("Z", &VectorZRef(mCameraPosition));
+
+		if (ImGui::Button("Reset Position"))
+		{
+			mCameraPosition = DEFAULT_CAMERA_POSITION;
+		}
+
+		ImGui::SliderAngle("Pitch", &VectorXRef(mCameraOrientation));
+		ImGui::SliderAngle("Yaw", &VectorYRef(mCameraOrientation));
+		ImGui::SliderAngle("Roll", &VectorZRef(mCameraOrientation));
+
+		if (ImGui::Button("Reset Orientation"))
+		{
+			mCameraOrientation = DEFAULT_CAMERA_ORIENTATION;
+		}
+
+	}
+	ImGui::End();
+
+	if (ImGui::Begin("Entity"))
+	{
+		int itemSelected = 0;
+		if (ImGui::ListBox("Entity", &itemSelected, mEntityNames.data(), mEntityNames.size()))
+		{
+			mNode->setEntity(&mEntities.at(mEntityNames[itemSelected]));
+		}
+
+		ImGui::DragFloat("X", &VectorXRef(mEntityPosition));
+		ImGui::DragFloat("Y", &VectorYRef(mEntityPosition));
+		ImGui::DragFloat("Z", &VectorZRef(mEntityPosition));
+
+		if (ImGui::Button("Reset Position"))
+		{
+			mEntityPosition = DEFAULT_ENTITY_POSITION;
+		}
+
+		ImGui::SliderAngle("Pitch", &VectorXRef(mEntityOrientation));
+		ImGui::SliderAngle("Yaw", &VectorYRef(mEntityOrientation));
+		ImGui::SliderAngle("Roll", &VectorZRef(mEntityOrientation));
+
+		if (ImGui::Button("Reset Orientation"))
+		{
+			mEntityOrientation = DEFAULT_ENTITY_ORIENTATION;
+		}
+
+		ImGui::DragFloat("X Scaling", &VectorXRef(mEntityScaling));
+		ImGui::DragFloat("Y Scaling", &VectorYRef(mEntityScaling));
+		ImGui::DragFloat("Z Scaling", &VectorZRef(mEntityScaling));
+
+		if (ImGui::Button("Reset Scaling"))
+		{
+			mEntityScaling = DEFAULT_ENTITY_SCALING;
+		}
+
+	}
+	ImGui::End();
 }
